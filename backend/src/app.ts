8370d7b927773fa513d44a214d0b81ee22521c8b
fritz007x <fritz007x@gmail.com>
2023-11-15 21:12:34 -0500

    Create Express server

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import { config } from "dotenv";
+import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+config();
+const app = express();
+
+//midlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//Remove it in production
+app.use(morgan("dev"));
+
+app.use("/api1/v1", appRouter);
+
+export default app;
