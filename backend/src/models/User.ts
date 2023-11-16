32db8df557bdd4cf52fbf92e498567601c1ece9f
fritz007x <fritz007x@gmail.com>
2023-11-15 21:32:04 -0500

    Define database user model

---
+++ backend/src/models/User.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { randomUUID } from "crypto";
+const chatSchema = new mongoose.Schema({
+    id: {type: String, default: randomUUID()},
+    role:{type: String, required:true},
+    content:{type: String, required: true},
+})
+const userSchema = new mongoose.Schema({
+name: {type:String, required: true}, 
+email: {type: String, required: true},
+password: {type: String, required: true},
+chats: [chatSchema],
+})
+
+//name
+//email
+//password
+export default mongoose.model("User", userSchema);
