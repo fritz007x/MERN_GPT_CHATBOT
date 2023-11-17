74e2eb672799171295e8d341362194951ba00771
fritz007x <fritz007x@gmail.com>
2023-11-16 22:51:05 -0500

    Add Logo

---
+++ frontend/src/components/shared/Logo.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import  Typography  from "@mui/material/Typography";
+
+const Logo = ()=>{
+        
+    return <div style={{display:"flex", marginRight:"auto", alignItems:"center", gap:"8px" 
+}}
+    >
+        <Link to={"/"}>
+            <img src="openai.png" alt= "openai" height={"30 px"} width={"30px"} className="image-inverted"/>
+       
+        </Link>
+        <Typography sx = {{display: {md: "block", sm:"none", xs:"none"},
+        mr:"auto", 
+        fontWeight:"800",
+        textShadow: "2px 2px 20px #000"
+    }}>
+        <span style={{fontSize:"20px"}}>MERN</span>-GPT
+    </Typography>
+    </div>
+}
+
+export default Logo;
