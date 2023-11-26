2aab31b6b931c7f89a70246f6cdcbfbe9bc268fd
fritz007x <fritz007x@gmail.com>
2023-11-25 22:54:12 -0500

    Corrects variable names

--- backend/src/config/openai-config.ts
+++ backend/src/config/openai-config.ts
@@ -2,8 +2,8 @@ import { Configuration } from "openai";
 
 export const configureOpenAI = () => {
   const config = new Configuration({
-    apiKey: process.env.OPEN_AI_SECRET,
-    organization: process.env.OPENAI_ORAGANIZATION_ID,
+    apiKey: process.env.OPENAI_SECRET,
+    organization: process.env.OPENAI_ORGANIZATION_ID,
   });
   return config;
 };
