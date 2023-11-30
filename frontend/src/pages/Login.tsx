b1477f40baab399b1c4fc88bff4c0c432766506b
fritz007x <fritz007x@gmail.com>
2023-11-29 22:18:25 -0500

    Implement navigation from Login to Chat page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Typography, Button } from "@mui/material";
 import { IoIosLogIn } from "react-icons/io";
 import CustomizedInput from "../components/shared/CustomizedInput";
 import { toast } from "react-hot-toast";
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const auth = useAuth();
-
+  const navigate = useNavigate();
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     try {
-      toast.loading("Sign In", { id: "login" });
+      toast.loading("Signing In", { id: "login" });
       await auth?.login(email, password);
       toast.success("Signed In Succesfully", { id: "login" });
     } catch (error) {
-      toast.error("Sign In Failed", { id: "login" });
+      console.log("This is the error:");
+      console.log(error);
+      toast.error("Signing In Failed", { id: "login" });
     }
   };
+  useEffect(() => {
+    if (auth?.user) {
+      return navigate("/chat");
+    }
+  });
   return (
     <Box width={"100%"} height={"100%"} display={"flex"} flex={"1"}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xm: "none" }}>
